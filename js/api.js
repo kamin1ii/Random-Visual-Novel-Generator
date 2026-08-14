44c1923ac3d826ea36328d2ace878c1d369d9f99
kamin1ii <bataraoezra@gmail.com>
2026-08-14 11:42:31 -0230

    update client visible path

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=20';
+import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=21';
 
 export async function vndbQuery(endpoint, body){
   const res = await fetch(API + '/' + endpoint, {
