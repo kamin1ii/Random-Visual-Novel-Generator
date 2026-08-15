d3570592223a09e4bc90f44862b0342a995c088b
kamin1ii <bataraoezra@gmail.com>
2026-08-15 09:41:59 -0230

    Search tags locally from a bundled VNDB dump instead of live API calls

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=33';
+import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=34';
 
 export async function vndbQuery(endpoint, body){
   const res = await fetch(API + '/' + endpoint, {
--- js/coverImage.js
+++ js/coverImage.js
@@ -1,5 +1,5 @@
-import { els } from './dom.js?v=33';
-import { SENSITIVE_THRESHOLD } from './constants.js?v=33';
+import { els } from './dom.js?v=34';
+import { SENSITIVE_THRESHOLD } from './constants.js?v=34';
 
 // Routes through our own /img/<path> proxy instead of VNDB directly. The proxy caches
 // each image in R2 on first request, so VNDB sees one request per unique cover total,
