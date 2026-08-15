f7d74ea8a740f11c90c87b45add0780f1bc7e833
kamin1ii <bataraoezra@gmail.com>
2026-08-15 06:57:43 -0230

    TEMP: preview cv.t thumbnail path

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-import { els } from './dom.js?v=32';
-import { state } from './state.js?v=32';
-import { runQuery } from './api.js?v=32';
-import { buildFilters, describeFilters } from './filters.js?v=32';
-import { resetFilterUI } from './filterControls.js?v=32';
-import { makeTagPicker } from './tagPicker.js?v=32';
-import { showCurrent, setStatus, renderActiveFilters } from './render.js?v=32';
-import { initRevealModal, closeRevealModal, isRevealModalOpen, resetRevealPreference } from './revealModal.js?v=32';
-import { SENSITIVE_THRESHOLD } from './constants.js?v=32';
+import { els } from './dom.js?v=33';
+import { state } from './state.js?v=33';
+import { runQuery } from './api.js?v=33';
+import { buildFilters, describeFilters } from './filters.js?v=33';
+import { resetFilterUI } from './filterControls.js?v=33';
+import { makeTagPicker } from './tagPicker.js?v=33';
+import { showCurrent, setStatus, renderActiveFilters } from './render.js?v=33';
+import { initRevealModal, closeRevealModal, isRevealModalOpen, resetRevealPreference } from './revealModal.js?v=33';
+import { SENSITIVE_THRESHOLD } from './constants.js?v=33';
 
 makeTagPicker(els.includeInput, els.includeSuggest, els.includeStatus, state.includeTags, els.includeChips, 'include');
 makeTagPicker(els.excludeInput, els.excludeSuggest, els.excludeStatus, state.excludeTags, els.excludeChips, 'exclude');
