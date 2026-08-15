dceac9e4cc537861abbbba576ee275f0871614d8
kamin1ii <bataraoezra@gmail.com>
2026-08-15 15:58:46 -0230

    Use absolute image URLs instead of relative paths to prevent www redirect detour

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=43';
+import { API, VN_FIELDS, PER_PAGE } from './constants.js?v=45';
 
 export async function vndbQuery(endpoint, body){
   const res = await fetch(API + '/' + endpoint, {
